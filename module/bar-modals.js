d8c9b5fa22c4127530721c6cce4158747d8e96be
Filip <fili4252@stud.kea.dk>
2022-01-13 16:26:19 +0100

    toggle active class on click on tap and remove active class from other taps

--- module/bar-modals.js
+++ module/bar-modals.js
@@ -1,7 +1,20 @@
 "use strict";
 
 export function registerClickOnTaps() {
-  document.querySelectorAll(".tap").forEach((tap) => {
-    tap.addEventListener("click", () => console.log("clicked on tap", tap));
+  const taps = document.querySelectorAll(".tap");
+  taps.forEach((tap) => {
+    tap.addEventListener("click", () => {
+      console.log("clicked on tap", tap);
+      toggleActiveElement(tap, taps);
+    });
   });
+}
+
+function toggleActiveElement(el, allEls) {
+  allEls.forEach((element) => {
+    if (el !== element) {
+      element.classList.remove("bar-el-active");
+    }
+  });
+  el.classList.toggle("bar-el-active");
 }
